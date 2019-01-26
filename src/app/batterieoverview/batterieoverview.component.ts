c75e3b6e49909d331f0f87b31617770652550c56
Markus Z <st114857@stud.uni.stuttgart.de>
2019-01-26 10:24:55 +0100

    linking functionality for batterie navigation

--- src/app/batterieoverview/batterieoverview.component.ts
+++ src/app/batterieoverview/batterieoverview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService} from '../data.service';
 import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-batterieoverview',
   templateUrl: './batterieoverview.component.html',
