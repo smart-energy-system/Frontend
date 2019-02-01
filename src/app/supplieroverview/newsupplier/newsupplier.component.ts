cf10d399a49b3de9cd62a2fde82ee9c6a3208180
Markus Z <st114857@stud.uni.stuttgart.de>
2019-02-01 14:21:38 +0100

    fixxed leaflet and sizing

--- src/app/supplieroverview/newsupplier/newsupplier.component.ts
+++ src/app/supplieroverview/newsupplier/newsupplier.component.ts
@@ -17,9 +17,9 @@ export class NewsupplierComponent implements OnInit {
 
   options = {
     layers: [
-//      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//        attribution: '&copy; OpenStreetMap contributors'
-//      })
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+      })
     ],
     zoom: 7,
     center: latLng([ 46.879966, -121.726909 ])
