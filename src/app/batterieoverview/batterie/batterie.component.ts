8dcba5b580fa604afdca54187e17c7c8f9a3d86a
Markus Z <st114857@stud.uni.stuttgart.de>
2019-02-04 16:56:22 +0100

    batterie update fixxed

--- src/app/batterieoverview/batterie/batterie.component.ts
+++ src/app/batterieoverview/batterie/batterie.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../data.service';
 import { ActivatedRoute} from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { latLng, tileLayer, marker, icon } from 'leaflet';
 
 @Component({
   selector: 'app-batterie',
@@ -9,9 +12,90 @@ import { ActivatedRoute} from '@angular/router';
 })
 export class BatterieComponent implements OnInit {
 
-  id: Object;
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+      })
+    ],
+    zoom: 6,
+    center: latLng([ 50, 11 ])
+  };
 
-  constructor(private data: DataService, private route: ActivatedRoute) { }
+  id: Object;
+  batterieForm: FormGroup;
+  submitted = false;
+  success = false;
+  layers;
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private route: ActivatedRoute) {
+    this.batterieForm = this.formBuilder.group({
+      displayname: ['' , Validators.required],
+      lat: ['', Validators.required],
+      long: ['', Validators.required],
+      maximumStoredEnergy: ['' , Validators.required],
+      maximumChargingRate: ['', Validators.required],
+      maximumDischargingRate: ['', Validators.required],
+      chargingEfficiency: ['', Validators.required],
+      storedEnergy: ['', Validators.required]
+    });
+  }
+
+  onUpdate(){
+    this.submitted = true;
+
+    console.log("batterie update clicked!");
+
+    if(this.batterieForm.invalid){
+      return;
+    }
+
+    let tempdata = {
+      displayName: this.batterieForm.get('displayname').value,
+      latitude: this.batterieForm.get('lat').value,
+      longitude: this.batterieForm.get('long').value,
+      chargingEfficiency: this.batterieForm.get('chargingEfficiency').value,
+      maximumChargingRate: this.batterieForm.get('maximumChargingRate').value,
+      maximumDischargingRate: this.batterieForm.get('maximumDischargingRate').value,
+      maximumStoredEnergy: this.batterieForm.get('maximumStoredEnergy').value,
+      storedEnergy: this.batterieForm.get('storedEnergy').value
+    };
+    let data = JSON.stringify(tempdata);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+
+    this.http.put("http://localhost:8090/supplier/batteries/"+this.id, data, httpOptions).subscribe();
+    this.success = true;
+  }
+
+  onLeafletClick(event){
+    this.layers = [marker(event.latlng,{icon: icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'leaflet/marker-icon.png',
+        shadowUrl: 'leaflet/marker-shadow.png'
+      })
+    })];
+  }
+
+  onExport(){
+    this.layers = [marker([this.batterieForm.get('lat').value , this.batterieForm.get('long').value],{icon: icon({
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
+      iconUrl: 'leaflet/marker-icon.png',
+      shadowUrl: 'leaflet/marker-shadow.png'
+      })
+    })];
+  }
+
+  onImport(){
+    this.batterieForm.controls['lat'].setValue(this.layers[0].getLatLng()['lat']);
+    this.batterieForm.controls['long'].setValue(this.layers[0].getLatLng()['lng']);
+  }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
